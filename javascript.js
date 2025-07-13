53a9897eacb5091a46df27b0f5186073186c8e78
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-07-13 16:53:00 +0300

    fix bug

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
-//get items
-const arrow = document.querySelectorAll('img');
-const headerItems = document.querySelectorAll('.header-items');
+const headerItems = document.querySelectorAll(".list-container");
+
+for (let i = 0; i < headerItems.length; i++) {
+  const dropdown = headerItems[i].querySelector(".dropdown");
+
+  headerItems[i].addEventListener("mouseenter", function () {
+    dropdown.classList.add("show");
+  });
+
+  headerItems[i].addEventListener("mouseleave", function () {
+    dropdown.classList.remove("show");
+  });
+}
